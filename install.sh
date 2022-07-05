ecdb1c6d9cf857a3c61cdf4cb200b0707c254cba
Gal Rotbard <galro@Gals-MacBook-Air.local>
2022-07-05 17:34:47 +0300

    notebook with wav signal

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
-#python3 -m venv corsound_venv
-#pip3 install -r requirements.txt
+python3 -m venv corsound_venv
 source corsound_venv/bin/activate
+pip3 install -r requirements.txt
+ipython kernel install --user --name=corsound_venv
 sudo apt install ffmpeg youtube-dl
-wget http://www.robots.ox.ac.uk/\~vgg/data/voxceleb/data/vox2_dev_txt.zip
-unzip vox2_dev_txt.zip
+wget https://www.robots.ox.ac.uk/~vgg/data/voxceleb/data/vox2_test_txt.zip
+unzip vox2_test_txt.zip
+#dev dataset (~6000 voices)
+# wget http://www.robots.ox.ac.uk/\~vgg/data/voxceleb/data/vox2_dev_txt.zip
+#unzip vox2_dev_txt.zip
