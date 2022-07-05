7d3338f4445c3ad23d0e9fd96797b9fdb09f110e
Gal Rotbard <sgalrot@alumni.technion.ac.il>
2022-07-05 15:53:46 +0300

    addind req.txt for linux

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
-brew install ffmpeg youtube-dl
+#python3 -m venv corsound_venv
+#pip3 install -r requirements.txt
+source corsound_venv/bin/activate
+sudo apt install ffmpeg youtube-dl
 wget http://www.robots.ox.ac.uk/\~vgg/data/voxceleb/data/vox2_dev_txt.zip
 unzip vox2_dev_txt.zip
