3ade4a21ee689e64d511d0d39ae27f7cb5969750
Gal Rotbard <galro@Gals-MacBook-Air.local>
2022-07-08 11:00:56 +0300

    adding cp fix file for luigi repo

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ipython kernel install --user --name=corsound_venv
 sudo apt install ffmpeg youtube-dl
 wget https://www.robots.ox.ac.uk/~vgg/data/voxceleb/data/vox2_test_txt.zip
 unzip vox2_test_txt.zip
+cp .//extract_segment.py.fix .//corsound_venv/lib/python3.9/site-packages/voxceleb_luigi/extract_segment.py
 #dev dataset (~6000 voices)
 # wget http://www.robots.ox.ac.uk/\~vgg/data/voxceleb/data/vox2_dev_txt.zip
 #unzip vox2_dev_txt.zip
